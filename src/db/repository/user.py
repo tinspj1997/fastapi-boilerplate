5203fe46801723c1da6d5b526c7240d9304b3abb
Tins P Joseph <tinspj1997@gmail.com>
2026-01-13 07:29:10 +0000

    decorator updated

--- src/db/repository/user.py
+++ src/db/repository/user.py
@@ -1,13 +1,16 @@
-from src.core.artifacts.decorator import inject_db
+from src.core.artifacts.decorator.db import inject_db
 from src.core.artifacts.interface.repository import IReposiitory
 from src.db.models import User
-from sqlalchemy import select,func
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func
+from sqlalchemy.orm import Session
+
 
 class UserRepository(IReposiitory):
     repository_name = "UserRepository"
-    
+
     @inject_db()
-    async def fetch_user_count(self,session:AsyncSession) -> int:
-        result = await session.execute(select(func.count()).select_from(User))
+    async def fetch_user_count(self, session: Session) -> int:
+        result = session.execute(
+            select(func.count()).select_from(User)
+        )
         return result.scalar_one()
